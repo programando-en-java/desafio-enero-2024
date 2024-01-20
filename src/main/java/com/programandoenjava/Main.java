c86168eccde7e10889cdca503a03d98e59c7182c
david-quinones <david.quinones@outlook.es>
2024-01-20 18:17:54 +0100

    part 1

--- src/main/java/com/programandoenjava/Main.java
+++ src/main/java/com/programandoenjava/Main.java
@@ -1,8 +1,13 @@
 package com.programandoenjava;
 
+import com.programandoenjava.parte1.Parte1;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        Parte1 parte1 = new Parte1();
+        System.out.println(parte1.invertirPalabras("david quinones"));
 
     }
 }
